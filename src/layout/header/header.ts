3475a7e1a7d5360405069a6cb842393e66dba220
Pablo Marzoa <pablomarzoalopez@gmail.com>
2026-04-30 17:22:37 +0200

    feat: add header tooltip

--- src/layout/header/header.ts
+++ src/layout/header/header.ts
@@ -2,14 +2,16 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { ThemeService } from '../../shared/theme/theme.service';
+import { TranslatePipe } from "../../shared/i18n/translate.pipe";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.html',
   styleUrl: './header.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatTooltipModule, TranslatePipe],
 })
 export class HeaderComponent {
   protected readonly themeService = inject(ThemeService);
